48c5b2aaafc8454acf074aed295ba2867ea4fd34
Péter Molnár <xodax9898@gmail.com>
2024-04-08 15:24:16 +0200

    feat(frontend) : addition of some basic components e.g.: button, dropdown-menu
    feat(pages): Creation of some basic page skeleton && Modification of existing pages

---
+++ src/pages/homePage.tsx
@@ -0,0 +1,14 @@
+import { Button } from '~/components/ui/button'
+import Link from "next/link";
+import  SignInBtn  from '~/components/auth/authButtons';
+
+
+const HomePage = () => {
+    return (
+        <>
+            <SignInBtn />
+        </>
+    )
+}
+
+export default HomePage;
---
+++ src/pages/testPage.tsx
@@ -0,0 +1,8 @@
+const testPage = () =>{
+    return (
+        <>
+        <p>This is the way we ball</p>
+        </>
+    )
+}
+export default testPage;
