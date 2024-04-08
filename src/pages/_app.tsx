cd96a54c76248a38db0b2a26c8559e49372c6c14
Péter Molnár <xodax9898@gmail.com>
2024-04-08 20:55:43 +0200

    del: removed unsued fonts Reason : It was  redundant, I have already defined it once

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { cn } from "~/lib/utils";
 import { Inter as FontSans } from "next/font/google"
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 const fontSans = FontSans({
   subsets: ["latin"],
