2dcd3d8564c714833203cfb008d7bce99e5b390e
Péter Molnár <xodax9898@gmail.com>
2024-04-08 21:11:34 +0200

    feat:Addition of the latest commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
