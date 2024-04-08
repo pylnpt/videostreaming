6df64f9de7d16b010d6b0222824b1472ae2f5a60
Péter Molnár <xodax9898@gmail.com>
2024-04-08 20:53:08 +0200

    feat(frontend): created a logo component It also has a functionality to navigate us to the homepage

---
+++ src/components/navigation-bar/logo.tsx
@@ -0,0 +1,26 @@
+import  Image  from "next/image";
+import Link from "next/link";
+import LogoIcon from "/public/logo.png"
+import { cn } from "~/lib/utils";
+
+export const Logo = () => {
+    return (
+        <>
+            <Link href="/">
+                <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
+                    <div className="bg-[#3DD68C] rounded-full p-1">
+                        <Image 
+                            src={LogoIcon} 
+                            alt="streamLogo"
+                            width={32}
+                            height={32}/>
+                    </div>
+                    <div>
+                        <p className="text-lg font-semibold">Thesis</p>
+                        <p className="text-xs text-muted-foreground">Streaming webapp with the use of AI</p>
+                    </div>
+                </div>
+            </Link>
+        </>
+    )
+}
