3c901e819ed343b2efd32e537a880ad6e3ecd47c
Péter Molnár <xodax9898@gmail.com>
2024-04-08 20:50:11 +0200

    del: removed unused AuthShowcase() function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "~/components/theme-provider"
 import  Header  from "~/components/head"
 import { Button } from "~/components/ui/button"
 
@@ -20,29 +19,5 @@ export default function Home() {
       <HomePage />
       {/* <AuthShowcase /> */}
     </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
   );
 }
